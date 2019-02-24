c14691836fbe34a2a72ff04839fb934dc11cb93e
Alex <abhbretones@gmail.com>
2019-02-24 12:44:57 +0100

    ADD basic routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
@@ -8,10 +9,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // Components
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './modules/main/pages/main.component';
+import { PostsComponent } from './modules/posts/pages/posts/posts.component';
+import { UsersComponent } from './modules/users/pages/users/users.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: MainComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'posts', component: PostsComponent }
+];
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent,
+    PostsComponent,
+    UsersComponent
+  ],
+  imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
