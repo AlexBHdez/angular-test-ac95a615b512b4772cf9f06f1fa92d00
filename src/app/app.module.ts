7b4ede1e80f3ca3b5b4a187303d43c74fc3d7e4c
Alex <abhbretones@gmail.com>
2019-02-24 19:49:14 +0100

    ADD user component in users comp.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './modules/main/pages/main.component';
 import { PostComponent } from './modules/posts/components/post/post.component';
 import { PostsComponent } from './modules/posts/pages/posts/posts.component';
+import { UserComponent } from './modules/users/components/user/user.component';
 import { UsersComponent } from './modules/users/pages/users/users.component';
 
 const routes: Routes = [
@@ -31,7 +32,8 @@ const routes: Routes = [
     MainComponent,
     PostsComponent,
     PostComponent,
-    UsersComponent
+    UsersComponent,
+    UserComponent
   ],
   imports: [BrowserModule,
     BrowserAnimationsModule,
